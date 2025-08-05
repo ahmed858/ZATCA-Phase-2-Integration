7efecb16309f3f70f2c96374130d3f1c9e0b4937
user <user@DESKTOP-TAIT2LP>
2025-08-05 16:28:52 +0300

    fetal: add try catch to send_invoice function

--- zatca_amaken/zatca_help_files/zatca calls/send_invoice.py
+++ zatca_amaken/zatca_help_files/zatca calls/send_invoice.py
@@ -3,20 +3,24 @@ import json
 import  utils.token as tkn
 
 def send_invoice(username,password,invoice_hash,UUID,encoded_invoice,URL):
-    token_base64 = tkn.get_authHeader(username,password)
-    headers = {
-                "Accept-Version": "v2",
-                "Accept-Language": "en",
-                "request-from": "zatca-service",
-                "Content-Type": "application/json",
-                "Authorization": f"Basic {token_base64}",
-            }
-    payload = json.dumps(
-                {
-                    "invoiceHash": invoice_hash,
-                    "uuid": UUID,
-                    "invoice": encoded_invoice,
+    try:
+        token_base64 = tkn.get_authHeader(username,password)
+        headers = {
+                    "Accept-Version": "v2",
+                    "Accept-Language": "en",
+                    "request-from": "zatca-service",
+                    "Content-Type": "application/json",
+                    "Authorization": f"Basic {token_base64}",
                 }
-            )
-    return requests.request("POST",url=URL,headers=headers,data=payload)
+        payload = json.dumps(
+                    {
+                        "invoiceHash": invoice_hash,
+                        "uuid": UUID,
+                        "invoice": encoded_invoice,
+                    }
+                )
+        return requests.request("POST",url=URL,headers=headers,data=payload)
+    except Exception as e:
+        frappe.throw(e)
+
     
